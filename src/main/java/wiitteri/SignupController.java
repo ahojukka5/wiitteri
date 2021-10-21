a2f8d30918c16a8ea17b24f6a1d3419ecaf99f86
Jukka Aho <ahojukka5@gmail.com>
2021-10-22 00:55:59 +0300

    Add SignupController.java

---
+++ src/main/java/wiitteri/SignupController.java
@@ -0,0 +1,27 @@
+package wiitteri;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SignupController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String post(@RequestParam String username, @RequestParam String password, @RequestParam String handle) {
+        logger.debug("username: " + username);
+        logger.debug("password length: " + password.length());
+        logger.debug("handle: " + handle);
+        return "redirect:/";
+    }
+}
