3a237c58436ee73d2b14b48dc99441887fb6533a
Jukka Aho <ahojukka5@gmail.com>
2021-10-22 18:03:06 +0300

    Add AccountRepository.java

---
+++ src/main/java/wiitteri/AccountRepository.java
@@ -0,0 +1,7 @@
+package wiitteri;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByUsername(String username);
+}
