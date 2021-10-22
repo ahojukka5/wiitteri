bed1d5860efd83e75f6192709dbd74de4c1d64c0
Jukka Aho <ahojukka5@gmail.com>
2021-10-22 20:50:25 +0300

    Add controller to show main stream /home

---
+++ src/main/java/wiitteri/HomeController.java
@@ -0,0 +1,20 @@
+package wiitteri;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("username", loginService.getUsername());
+        return "home";
+    }
+
+}
