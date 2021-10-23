d788b395dc3a5127bff14d2daddcae70e07e3008
Jukka Aho <ahojukka5@gmail.com>
2021-10-23 12:03:33 +0300

    Use SearchService to find by username

--- src/main/java/wiitteri/SearchController.java
+++ src/main/java/wiitteri/SearchController.java
@@ -1,5 +1,6 @@
 package wiitteri;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SearchController {
 
+    @Autowired
+    private SearchService searchService;
+
     @GetMapping("/search")
     public String serch(Model model, @RequestParam String q) {
         model.addAttribute("searchString", q);
+        model.addAttribute("users", searchService.search(q));
         return "search";
     }
 
