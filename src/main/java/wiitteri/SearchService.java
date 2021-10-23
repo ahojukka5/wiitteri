b571ae637b856bc35c69c77c474c0e6160e18b1f
Jukka Aho <ahojukka5@gmail.com>
2021-10-23 12:02:57 +0300

    Implement SearchService.java

---
+++ src/main/java/wiitteri/SearchService.java
@@ -0,0 +1,23 @@
+package wiitteri;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public List<Account> search(String query) {
+        logger.debug("Searching by username, query string = " + query);
+        return accountRepository.findByUsernameContainingIgnoreCase(query);
+    }
+
+}
