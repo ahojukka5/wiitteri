28759cb371a2efaf823a9ec01a4b5a1a4780633f
Jukka Aho <ahojukka5@gmail.com>
2021-10-23 13:41:58 +0300

    HomeController: add attribute followedUsers to /home

--- src/main/java/wiitteri/HomeController.java
+++ src/main/java/wiitteri/HomeController.java
@@ -1,5 +1,10 @@
 package wiitteri;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +13,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private LoginService loginService;
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping("/home")
     public String home(Model model) {
-        model.addAttribute("username", loginService.getUsername());
+        String username = loginService.getUsername();
+        List<Account> followedUsers = accountService.getFollowedUsers();
+        logger.debug("logged in user: " + username);
+        logger.debug("user is following number of other users: " + followedUsers.size());
+        model.addAttribute("username", username);
+        model.addAttribute("followedUsers", followedUsers);
         return "home";
     }
 
