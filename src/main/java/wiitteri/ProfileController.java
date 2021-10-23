168b415ae630e8faff70c1a864fb76f14a44e571
Jukka Aho <ahojukka5@gmail.com>
2021-10-23 13:40:58 +0300

    Add route /{username}/follow to ProfileController

--- src/main/java/wiitteri/ProfileController.java
+++ src/main/java/wiitteri/ProfileController.java
@@ -1,5 +1,8 @@
 package wiitteri;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +11,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class ProfileController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    AccountService accountService;
+
     @GetMapping("/{username}")
     public String profile(Model model, @PathVariable String username) {
         model.addAttribute("username", username);
         return "profile";
     }
 
+    @GetMapping("/{username}/follow")
+    public String follow(Model model, @PathVariable String username) {
+        logger.debug("Following user " + username);
+        accountService.follow(username);
+        model.addAttribute("username", username);
+        return "redirect:/" + username;
+    }
 }
