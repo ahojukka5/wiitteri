a8e03358864d5534b69cb353acac973dc7e26be8
Jukka Aho <ahojukka5@gmail.com>
2021-10-23 13:35:24 +0300

    Add followedUsers to Account

--- src/main/java/wiitteri/Account.java
+++ src/main/java/wiitteri/Account.java
@@ -1,6 +1,10 @@
 package wiitteri;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
@@ -13,7 +17,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Account extends AbstractPersistable<Long> {
+
     private String username;
     private String passwordHash;
     private String handle;
+
+    @OneToMany
+    private List<Account> followedUsers;
+
+    public Account(String username2, String password, String handle2) {
+        this.username = username2;
+        this.passwordHash = "";
+        this.handle = handle2;
+        this.followedUsers = new ArrayList<>();
+    }
 }
