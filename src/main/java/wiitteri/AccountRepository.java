ba5263d18561b881ef1ea73924a50afd76507ad4
Jukka Aho <ahojukka5@gmail.com>
2021-10-23 12:02:29 +0300

    Search users by username case-insensitively

--- src/main/java/wiitteri/AccountRepository.java
+++ src/main/java/wiitteri/AccountRepository.java
@@ -1,7 +1,11 @@
 package wiitteri;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByUsername(String username);
+
+    List<Account> findByUsernameContainingIgnoreCase(String username);
 }
