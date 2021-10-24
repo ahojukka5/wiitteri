1dfb1abdea90f869fd504f6a4be71c0fe5ed19a6
Jukka Aho <ahojukka5@gmail.com>
2021-10-24 15:28:54 +0300

    ConnectionRepository: add new query to find connection

--- src/main/java/wiitteri/ConnectionRepository.java
+++ src/main/java/wiitteri/ConnectionRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConnectionRepository extends JpaRepository<Connection, Long> {
 
+    Connection findByFromAndTo(User from, User to);
+
 }
