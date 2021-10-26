6ed3d907020f637e513e06154c95e3abd90f4673
Jukka Aho <ahojukka5@gmail.com>
2021-10-26 18:06:38 +0300

    Add ImageController

---
+++ src/main/java/wiitteri/ImageController.java
@@ -0,0 +1,49 @@
+package wiitteri;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class ImageController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    @GetMapping("/{username}/images")
+    public String listImages(Model model, @PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+        model.addAttribute("username", username);
+        model.addAttribute("images", imageRepository.findByOwner(user));
+        return "images";
+    }
+
+    @GetMapping(path = "/images/{id}", produces = "image/png")
+    @ResponseBody
+    public byte[] getImage(@PathVariable Long id) {
+        return imageRepository.getOne(id).getContent();
+    }
+
+    @PostMapping("/{username}/images")
+    public String save(@PathVariable String username, @RequestParam MultipartFile file,
+            @RequestParam String description) throws IOException {
+        User owner = userService.getLoggedUser();
+        Image image = new Image(description, owner, file.getBytes());
+        imageRepository.save(image);
+        return "redirect:/" + owner.getUsername() + "/images";
+    }
+}
