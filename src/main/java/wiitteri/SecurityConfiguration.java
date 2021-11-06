3a2b05ff93c76d82621a603b0c31a03327f9b4c6
Jukka Aho <ahojukka5@gmail.com>
2021-11-06 15:00:42 +0200

    Add order to SecurityConfiguration

--- src/main/java/wiitteri/SecurityConfiguration.java
+++ src/main/java/wiitteri/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package wiitteri;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Order(1000)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
