e6eee4de2fac1c09724e4025b7ef6a0ad3679ec3
Jukka Aho <ahojukka5@gmail.com>
2021-11-09 00:37:48 +0200

    Implement ... IN ... query to TweetRepository

--- src/main/java/wiitteri/repositories/TweetRepository.java
+++ src/main/java/wiitteri/repositories/TweetRepository.java
@@ -1,9 +1,16 @@
 package wiitteri.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import wiitteri.models.Tweet;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+    List<Tweet> findByOwnerIdIn(List<Long> tweeterIds);
+
+    List<Tweet> findByOwnerIdIn(List<Long> tweeterIds, Pageable p);
 
 }
