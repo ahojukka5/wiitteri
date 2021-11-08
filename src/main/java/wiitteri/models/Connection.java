9893c871034b671e0545b5f93fa873b8521ab39d
Jukka Aho <ahojukka5@gmail.com>
2021-11-08 18:48:59 +0200

    Add UniqueConstraint to Connection (follow only once)

--- src/main/java/wiitteri/models/Connection.java
+++ src/main/java/wiitteri/models/Connection.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "from_user_fk", "to_user_fk" }) })
 public class Connection extends AbstractPersistable<Long> {
 
     @ManyToOne
