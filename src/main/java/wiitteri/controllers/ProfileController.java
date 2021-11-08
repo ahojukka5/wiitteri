b764d366e38c699f03f19cc3a656abe8b001c72d
Jukka Aho <ahojukka5@gmail.com>
2021-11-08 19:03:02 +0200

    ProfileController: use flash attribute to give information on block

--- src/main/java/wiitteri/controllers/ProfileController.java
+++ src/main/java/wiitteri/controllers/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import wiitteri.services.AccountService;
 
@@ -32,10 +33,11 @@ public class ProfileController {
         return "redirect:/" + username;
     }
 
-    @GetMapping("/profiles/{username}/block")
-    public String block(Model model, @PathVariable String username) {
-        logger.debug("Blocking user " + username);
-        userService.block(username);
+    @GetMapping("/profiles/{handle}/block")
+    public String block(Model model, @PathVariable String handle, RedirectAttributes redirectAttributes) {
+        logger.debug("Blocking user @" + handle);
+        accountService.block(handle);
+        redirectAttributes.addFlashAttribute("infoMessage", "User @" + handle + " is blocked");
         return "redirect:/home";
     }
 }
