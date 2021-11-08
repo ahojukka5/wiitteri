2a9ad7b326b594e3b964bccfd77e9aa10efd25c3
Jukka Aho <ahojukka5@gmail.com>
2021-11-08 18:59:08 +0200

    HomeController: return images and numberOfImages as attributes

--- src/main/java/wiitteri/controllers/HomeController.java
+++ src/main/java/wiitteri/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package wiitteri.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import wiitteri.models.Image;
 import wiitteri.services.AccountService;
 
 @Controller
@@ -18,6 +21,9 @@ public class HomeController {
         model.addAttribute("handle", accountService.getHandle());
         model.addAttribute("following", accountService.getFollowing());
         model.addAttribute("followers", accountService.getFollowers());
+        List<Image> images = accountService.getImages();
+        model.addAttribute("images", images);
+        model.addAttribute("numberOfImages", images.size());
         return "home";
     }
 
